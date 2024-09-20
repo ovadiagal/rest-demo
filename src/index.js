373758b193f72ce476386b8a44fe8f650cd96efb
Gal Ovadia <gal.ovadia@gatech.edu>
2024-09-19 22:52:54 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -5,57 +5,50 @@ const { randomInt } = require("crypto");
 const app = express();
 app.listen(3000);
 
+const DB = JSON.parse(fs.readFileSync("src/items.json", "utf8"));
+
 app.get("/", (req, res) => {
   res.send("REST assignment for CS3300");
 });
 
 app.get("/api/products", (req, res) => {
-  fs.readFile("src/items.json", "utf8", (err, data) => {
-    res.json(JSON.parse(data));
-  });
+  res.json(DB);
 });
 
 app.get("/api/products/:id", (req, res) => {
-  const id = req.params.id;
-  fs.readFile("src/items.json", "utf8", (err, data) => {
-    const products = JSON.parse(data);
-    const product = products.find((p) => p.id == parseInt(id));
-    res.json(product);
-  });
+  if (!DB[req.params.id]) {
+    return res.status(404).send("Product not found");
+  }
+  res.json(DB[req.params.id]);
 });
 
 app.post("/api/products", express.json(), (req, res) => {
-  fs.readFile("src/items.json", "utf8", (err, data) => {
-    const products = JSON.parse(data);
-    const newProduct = req.body;
-    newProduct.id = randomInt(1, 1000);
-    products.push(newProduct);
-    fs.writeFile("src/items.json", JSON.stringify(products), () => {
-      res.send(`${req.body["name"]} created successfully`);
-    });
-  });
+  let id;
+  do {
+    id = randomInt(1, 100);
+  } while (DB[id]);
+  DB[String(id)] = {
+    name: req.body.name,
+    quantity: req.body.quantity,
+  };
+  res.status(201).send("Product created successfully");
 });
 
 app.put("/api/products/:id", express.json(), (req, res) => {
-  const id = req.params.id;
-  fs.readFile("src/items.json", "utf8", (err, data) => {
-    const products = JSON.parse(data);
-    const index = products.findIndex((p) => p.id == parseInt(id));
-    products[index] = { ...products[index], ...req.body };
-    fs.writeFile("src/items.json", JSON.stringify(products), () => {
-      res.send(`${req.body["name"]} updated successfully`);
-    });
-  });
+  if (!DB[req.params.id]) {
+    return res.status(404).send("Product not found");
+  }
+  DB[req.params.id] = {
+    name: req.body.name ?? DB[req.params.id].name,
+    quantity: req.body.quantity ?? DB[req.params.id].quantity,
+  };
+  res.send("Product updated successfully");
 });
 
 app.delete("/api/products/:id", (req, res) => {
-  const id = req.params.id;
-  fs.readFile("src/items.json", "utf8", (err, data) => {
-    const products = JSON.parse(data);
-    const index = products.findIndex((p) => p.id === id);
-    products.splice(index, 1);
-    fs.writeFile("src/items.json", JSON.stringify(products), () => {
-      res.send("Product deleted successfully");
-    });
-  });
+  if (!DB[req.params.id]) {
+    return res.status(404).send("Product not found");
+  }
+  delete DB[req.params.id];
+  res.send("Product deleted successfully");
 });
